eb3025917b318025dd18f56d29b705b14ad878e3
Isroiljon <65494864+Isroiljoncyber@users.noreply.github.com>
2021-04-23 02:00:41 +0500

    We must add IoC Container

---
+++ WisePharm.Finance/IoC/IoC.cs
@@ -0,0 +1,15 @@
+﻿
+namespace WisePharm.Finance
+{
+    /// Create by Mr.Cyber
+	///<summarly>
+    /// This is IoC container for our application
+    ///</summarly>
+    public static class IoC
+    {
+        #region Constructor
+
+
+        #endregion
+    }
+}
