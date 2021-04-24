87ade729f5fda17608192ed82508607e4806acc9
Isroiljon <65494864+Isroiljoncyber@users.noreply.github.com>
2021-04-24 14:12:04 +0500

    Adding and modifing animation

---
+++ WisePharm.Finance/Animation/AnimationSlideDirection.cs
@@ -0,0 +1,24 @@
+﻿
+namespace WisePharm.Finance.Animation
+{
+    /// Create by Mr.Cyber
+	///<summarly>
+    /// This class is control all Animation 
+    ///</summarly>
+    public enum AnimationSlideDirection
+    {
+
+        /// Slide in from the left
+        Left = 1,
+
+        /// Slide in from the right
+        Right = 2,
+
+        /// Slide in from the top
+        Top = 3,
+
+        /// Slide in from the bottom
+        Bottom = 4
+
+    }
+}
---
+++ WisePharm.Finance/Animation/PageAnimation.cs
@@ -0,0 +1,26 @@
+﻿
+namespace WisePharm.Finance.Animation
+{
+    /// Create by Mr.Cyber
+	///<summarly>
+    /// Style of page animation for appearing/disappearing
+    ///</summarly>
+    public enum PageAnimation
+    {
+
+        /// <summary>
+        /// No animation take place 
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// The page slides in and fades in from the right
+        /// </summary>
+        SlideAndFadeInFromRight = 1,
+
+        /// <summary>
+        /// The page slides out anf fades out to the left
+        /// </summary>
+        SlideAndFadeOutFromLeft = 2,
+    }
+}
