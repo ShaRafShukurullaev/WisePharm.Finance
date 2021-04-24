38ff4804cd7721a50dc79bfcef17db0e4eab5d64
Isroiljon <65494864+Isroiljoncyber@users.noreply.github.com>
2021-04-24 13:56:33 +0500

    I add SVG images

---
+++ WisePharm.Finance/Images/SVGs/HomeSVG.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WisePharm.Finance
+{
+    /// <summary>
+    /// Interaction logic for LogoSVG.xaml
+    /// </summary>
+    public partial class HomeSVG : UserControl
+    {
+        public HomeSVG()
+        {
+            InitializeComponent();
+
+        }
+
+        /// <summary>
+        /// Dependency property to change fill color 
+        /// </summary>
+        public Brush BGColor
+        {
+            get { return (Brush)GetValue(BGColorProperty); }
+            set { SetValue(BGColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for BGColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty BGColorProperty =
+            DependencyProperty.Register("BGColor", typeof(Brush), typeof(HomeSVG));
+
+    }
+}
---
+++ WisePharm.Finance/Images/SVGs/LogOutSVG.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WisePharm.Finance
+{
+    /// <summary>
+    /// Interaction logic for LogoSVG.xaml
+    /// </summary>
+    public partial class LogOutSVG : UserControl
+    {
+        public LogOutSVG()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Dependency property to change fill color 
+        /// </summary>
+        public Brush BGColor
+        {
+            get { return (Brush)GetValue(BGColorProperty); }
+            set { SetValue(BGColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for BGColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty BGColorProperty =
+            DependencyProperty.Register("BGColor", typeof(Brush), typeof(LogOutSVG));
+
+    }
+}
